8c459e2c9a9cfeffefe5395a3f1aef6b340b5518
Kumar Gaurav <kumarg9695@gmail.com>
2024-06-03 00:38:55 +0530

    Created React and Vite React projects.

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>This is Kumar Gaurav. Hello World! Chai aur React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Chai aur React | Kumar Gaurav</h1>
+  )
+}
+
+export default App
