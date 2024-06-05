0e99ec1378db8c7a11eb0023b7e2a4194fd38436
Kumar Gaurav <kumarg9695@gmail.com>
2024-06-06 02:05:17 +0530

    Component Addition - Working Done.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,11 @@
+import Chai from "./Chai"
+
 function App() {
   return (
-    <h1>Chai aur React | Kumar Gaurav</h1>
+    <>
+      <Chai />
+      <h1>Chai aur React | Kumar Gaurav</h1>
+    </>
   )
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h3>Gaurav's chai is ready!</h3>
+    );
+}
+
+export default Chai
